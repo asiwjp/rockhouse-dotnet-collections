edd9aa5711faf53e37f02af4b481953681ec0dbd
akira saito <21022036+asiwjp@users.noreply.github.com>
2023-06-26 19:33:27 +0900

    Added Create method to Slot class.

---
+++ RockHouse.Collections.Tests/Slots/SlotTest.cs
@@ -0,0 +1,23 @@
+﻿using RockHouse.Collections.Slots;
+using Xunit;
+
+namespace RockHouse.Collections.Tests.Slots
+{
+    public class SlotTest
+    {
+        [Fact]
+        public void Test_Create_1()
+        {
+            var actual = Slot.Create("1");
+            Assert.Equal("1", actual.Item1);
+        }
+
+        [Fact]
+        public void Test_Create_2()
+        {
+            var actual = Slot.Create("1", "2");
+            Assert.Equal("1", actual.Item1);
+            Assert.Equal("2", actual.Item2);
+        }
+    }
+}
---
+++ RockHouse.Collections/Slots/Slot.cs
@@ -0,0 +1,30 @@
+﻿namespace RockHouse.Collections.Slots
+{
+    public static class Slot
+    {
+        /// <summary>
+        /// Create instance by the specified arguments.
+        /// </summary>
+        /// <typeparam name="T1">The type of the item1.</typeparam>
+        /// <param name="item1">Initial value of Item1.</param>
+        /// <returns>New Slot instance.</returns>
+        public static Slot<T1> Create<T1>(T1 item1)
+        {
+            return new Slot<T1>(item1);
+        }
+
+        /// <summary>
+        /// Create instance by the specified arguments.
+        /// </summary>
+        /// <typeparam name="T1">The type of the item1.</typeparam>
+        /// <typeparam name="T2">The type of the item2.</typeparam>
+        /// <param name="item1">Initial value of Item1.</param>
+        /// <param name="item2">Initial value of Item2.</param>
+        /// <returns>New Slot instance.</returns>
+        public static Slot<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
+        {
+            return new Slot<T1, T2>(item1, item2);
+        }
+
+    }
+}
